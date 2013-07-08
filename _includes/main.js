ed94b494d1d3a4ecbadc7fcad99f10ee9058cf70
Quico Moya <me@qmoya.com>
2013-07-08 10:51:33 +0200

    Add Jekyll skeleton

---
+++ _includes/main.js
@@ -0,0 +1,9 @@
+/*jslint browser: true, devel: true*/
+
+window.addEventListener("load", function () {
+    'use strict';
+
+    window.addEventListener("touchstart", function (event) {
+        event.preventDefault();
+    });
+});
