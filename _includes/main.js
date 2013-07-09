5b508c21f669b6fd3d0d9882af064403ba57a3fd
Quico Moya <me@qmoya.com>
2013-07-09 17:53:19 +0200

    Clean up

--- _includes/main.js
+++
@@ -1,9 +0,0 @@
-/*jslint browser: true, devel: true*/
-
-window.addEventListener("load", function () {
-    'use strict';
-
-    window.addEventListener("touchstart", function (event) {
-        event.preventDefault();
-    });
-});
